28579e37de29ebd4cb1e9efb9edddda6c3adc5c6
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-10-27 15:21:26 -0500

    rename script to keep consistency

--- src/date_functions.jl
+++
@@ -1,9 +0,0 @@
-
-struct BDay <: DatePeriod
-    value::Int64
-    calendar::Union{Symbol, String}
-    BDay(v::Number, cal::Union{Symbol, String}) = new(v, cal)
-end
-
-Base.:(+)(dt::Date, z::BDay) = advancebdays(z.calendar, dt, z.value)
-Base.:(-)(dt::Date, z::BDay) = advancebdays(z.calendar, dt, -1 * z.value)
